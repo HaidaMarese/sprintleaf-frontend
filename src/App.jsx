07a6808b1a9ec4385155ab66c3661bd5f1ff2315
Haida Makouangou <hmakouan06@gmail.com>
2025-08-04 16:35:35 -0400

    Fix case-sensitive import paths for Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Tasks from "./pages/Tasks";
-import Courses from "./pages/Courses";
+import Courses from "./pages/courses";
 import Notes from "./pages/Notes";
 import CalendarPage from "./pages/CalendarPage";
 import PrivateRoute from "./components/PrivateRoute";
